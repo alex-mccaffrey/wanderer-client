dade866f895afb950d6a310501abd89f2d1102e3
Alex McCaffrey <alex.mccaffrey07@gmail.com>
2021-05-20 07:22:24 -0600

    took out url of icon

--- src/Map/FullMap/FullMap.js
+++ src/Map/FullMap/FullMap.js
@@ -128,11 +128,10 @@ export default function FullMap() {
             key={tempMarker.time}
             //anchor={{ lat: tempMarker.lat, lng: tempMarker.lng }}
             position={{ lat: tempMarker.lat, lng: tempMarker.lng }}
-            icon={{
-              url: "http://maps.google.com/mapfiles/ms/icons/blue.png",
+            icon={"http://maps.google.com/mapfiles/ms/icons/blue.png"
               // origin: new window.google.maps.Point(0, 0),
               // anchor: new window.google.maps.Point(22, 22),
-            }}
+            }
             onClick={() => {
               setSelected(tempMarker);
             }}
